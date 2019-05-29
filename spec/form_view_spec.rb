60e92980ab814d34a3c2ba0c0a0e5096832ed9a6
Anthony Hull <m@anthonyhull.me>
2019-05-29 13:42:38 +0100

    birthdate form and submit

--- spec/form_view_spec.rb
+++ spec/form_view_spec.rb
@@ -9,4 +9,13 @@ describe 'birthday page' do
   it 'has the call to action Enter your birthday' do
     expect(page).to have_content 'Enter your birthday'
   end
+  it 'shows name of person' do
+    within('form') do
+      fill_in 'name', with: 'Anthony'
+      fill_in 'month', with: '11'
+      fill_in 'day', with: '01'
+    end
+    click_button 'Submit'
+    expect(page).to have_content 'Your birthday will be in:'
+  end
 end
